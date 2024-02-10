355edb49e759dcac9eb6822c432299b36e137c91
Nadilla C. Putri <58943422+nadillachantika@users.noreply.github.com>
2024-02-10 09:08:00 +0700

    auth login logout

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'John Doe',
+                'email' => 'john@example.com',
+                'password' => Hash::make('password123'),
+                'role' => 'admin',
+            ],
+            [
+                'name' => 'Jane Doe',
+                'email' => 'jane@example.com',
+                'password' => Hash::make('password123'),
+                'role' => 'staff',
+            ],
+            [
+                'name' => 'Alice Smith',
+                'email' => 'alice@example.com',
+                'password' => Hash::make('password123'),
+                'role' => 'user',
+            ],
+            [
+
+            ]
+        ]);
+    }
+}
